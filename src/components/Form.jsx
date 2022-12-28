4b9d5e0a58c9f3ad6dc45e8f7d77830ad1b823e1
Jaco-Miras <miras.jaco@gmail.com>
2022-12-29 00:36:22 +0800

    refactor

---
+++ src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo.id === id ? { title, id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo("");
+  };
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidV4(), title: input, completed: false }]);
+      setInput("");
+    } else {
+      updateTodo(input, editTodo.id, editTodo.completed);
+    }
+  };
+
+  useEffect(() => {
+    if (editTodo) {
+      setInput(editTodo.title);
+    } else {
+      setInput("");
+    }
+  }, [setInput, editTodo]);
+
+  console.log(todos);
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a Todo"
+        className="task-input"
+        value={input}
+        required
+        onChange={onInputChange}
+      />
+      <button className="add-button">{editTodo ? "OK" : "Add"}</button>
+    </form>
+  );
+};
+
+export default Form;
